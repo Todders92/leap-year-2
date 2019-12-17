6c898ac54ebcbb3467c27b4eead148af96bd5dd6
Student <example@example.com>
2019-12-16 18:16:45 -0800

    Create leapyear detector with logic that all years evaluate to false

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Back-end logic:
+
+var leapYear = function(year) {
+  return false;
+};
+
+// Front-end logic:
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
